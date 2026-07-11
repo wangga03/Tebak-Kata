a4f7d083c3e3aa2dc60881518555ff8a003a511c
wangga03 <aswanggasadu@gmail.com>
2026-07-12 01:49:24 +0700

    Fix SSL using String URL

--- api/db.js
+++ api/db.js
@@ -1,27 +1,12 @@
 const mysql = require('mysql2/promise');
 
-// 1. Ekstrak informasi dari URL Vercel secara manual
-const dbUrl = new URL(process.env.MYSQL_DATABASE_URL);
-const dbPassword = decodeURIComponent(dbUrl.password); // Mencegah karakter spesial rusak
+let url = process.env.MYSQL_DATABASE_URL;
 
-// 2. Buat koneksi secara absolut (memaksa SSL aktif)
-const pool = mysql.createPool({
-    host: dbUrl.hostname,
-    port: dbUrl.port || 4000,
-    user: dbUrl.username,
-    password: dbPassword,
-    database: dbUrl.pathname.replace('/', '') || 'test',
-    ssl: {
-        minVersion: 'TLSv1.2',
-        rejectUnauthorized: true
-    },
-    waitForConnections: true,
-    connectionLimit: 10,
-    maxIdle: 10,
-    idleTimeout: 60000,
-    queueLimit: 0,
-    enableKeepAlive: true,
-    keepAliveInitialDelay: 0
-});
+// Pastikan parameter keamanan SSL ada di dalam URL
+if (url && !url.includes('ssl=')) {
+    url += (url.includes('?') ? '&' : '?') + 'ssl={"rejectUnauthorized":true}';
+}
+
+const pool = mysql.createPool(url);
 
 module.exports = pool;
